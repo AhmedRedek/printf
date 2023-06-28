f8a8bf55cfbfeb46d80f9cc81e1a5249d178ad00
AhmedRedek <ahmedkeder91@gmail.com>
2023-06-27 17:03:14 -0700

    Ahmed

--- _printf.c
+++ _printf.c
@@ -1,1 +1,42 @@
-Ahsre
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int count = 0;
+    const char *ptr = format;
+
+    while (*ptr != '\0') {
+        if (*ptr == '%') {
+            ptr++;  // Move past '%'
+            if (*ptr == '\0')
+                break;
+
+            if (*ptr == 'c') {
+                int ch = va_arg(args, int);
+                putchar(ch);
+                count++;
+            } else if (*ptr == 's') {
+                const char *str = va_arg(args, const char *);
+                while (*str != '\0') {
+                    putchar(*str);
+                    str++;
+                    count++;
+                }
+            } else if (*ptr == '%') {
+                putchar('%');
+                count++;
+            }
+        } else {
+            putchar(*ptr);
+            count++;
+        }
+        ptr++;
+    }
+
+    va_end(args);
+    return count;
+}
